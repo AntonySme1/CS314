5c4810b6f372538327041fbf03ba20c5d55e2314
Saurav Bhuju <sbhuju@rams.colostate.edu>
2019-02-20 12:35:17 -0700

    parsed json file and used console.log() display #101

--- client/src/components/Application/ItineraryForm/ItineraryForm.js
+++ client/src/components/Application/ItineraryForm/ItineraryForm.js
@@ -7,7 +7,7 @@ const ItineraryForm = (props) => {
 
             <FormGroup>
                 <Label for="itinerary">Itinerary Upload</Label>
-                <Input type="file" name="Itinerary Upload" id="itinerary" accept=".json,application/json" onChange ={test}/>
+                <Input type="file" name="Itinerary Upload" id="itinerary" accept=".json,application/json" onChange ={readFile}/>
                 <FormText color="muted">
                     Please upload '.json' extension file only.
                 </FormText>
@@ -18,20 +18,55 @@ const ItineraryForm = (props) => {
     );
 }
 
-
-const test = (event) => {
-
-    const file = event.target.files[0];
-
+const processFile = (file) => {
     const reader = new FileReader();
 
-    reader.onload = () => {
-        console.log (reader.result);
-        }
+    return new Promise((resolve => {
+        reader.onload = () => {
+            resolve(reader.result);
+        };
 
-    reader.readAsText(file);
+        reader.readAsText(file);
 
+    }));
+}
+
+const readFile =  async (event) => {
+    const file = event.target.files[0];
+    const fileContent = await processFile (file);
+    printJSON(fileContent);
+}
+
+const printJSON = (fileContent) => {
+   const parsedJSON = JSON.parse(fileContent);
+   console.log("requestType is ", parsedJSON.requestType);
+   console.log("requestVersion is ", parsedJSON.requestVersion);
+   console.log("options is ", parsedJSON.options);
+   console.log("places is ", parsedJSON.places);
+   console.log("distances is ", parsedJSON.distances);
 
 }
+
+
+
+//code from https://stackoverflow.com/questions/3710204/how-to-check-if-a-string-is-a-valid-json-string-in-javascript-without-using-try
+//Author: Lynn and Matt H.
+const tryParseJSON = (jsonString) =>{
+    try {
+        const o = JSON.parse(jsonString);
+
+        if (o && typeof o === "object") {
+            return true;
+        }
+    }
+    catch (e) { }
+
+    return false;
+};
+
+
+
+
+
 export default ItineraryForm;
 
