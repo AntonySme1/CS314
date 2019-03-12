d31e56b674d9c8887bcb6f4810dbb6a95a29a51b
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-03-12 11:01:54 -0600

    Added map existence test to Itinerary.test.js

--- client/test/Itinerary.test.js
+++ client/test/Itinerary.test.js
@@ -3,8 +3,9 @@ import React from 'react';
 import {mount} from 'enzyme';
 import Itinerary from '../src/components/Application/Itinerary/Itinerary';
 import ErrorBanner from '../src/components/Application/ErrorBanner';
+import Geolocation from '../src/components/Application/Geolocation'
 import {getOriginalServerPort} from '../src/api/restfulAPI';
-import {Map, Marker, Popup, TileLayer} from "react-leaflet";
+import {Map, TileLayer} from "react-leaflet";
 
 const startProperties = {
     serverConfig: null,
@@ -30,6 +31,17 @@ function mapExistenceTest() {
     const itinerary = mount(<Itinerary options={startProperties.planOptions}
                                        settings={startProperties.clientSettings}
                                        createErrorBanner={createErrorBanner}/>);
+
+    expect(itinerary.containsMatchingElement(
+        <Map center={L.latLng(40.576179, -105.080773)}
+             zoom={10}
+             style={{height: 500, maxwidth: 700}}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                       attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            />
+            <Geolocation/>
+        </Map>
+    )).toEqual(true);
 }
 
 test('Testing the renderMap() function in the Itinerary component', mapExistenceTest);
