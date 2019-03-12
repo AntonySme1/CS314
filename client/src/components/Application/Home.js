2370c0280609d34daa8bb1a789e798d063e41ae4
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-03-12 08:19:22 -0600

    Added Geolocation component to Home.js

--- client/src/components/Application/Home.js
+++ client/src/components/Application/Home.js
@@ -5,6 +5,7 @@ import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import 'leaflet/dist/leaflet.css';
 import { Map, Marker, Popup, TileLayer, Polyline} from 'react-leaflet';
 import Pane from './Pane'
+import Geolocation from './Geolocation';
 
 /*
  * Renders the home page.
@@ -57,15 +58,12 @@ export default class Home extends Component {
     // 1: bounds={this.coloradoGeographicBoundaries()}
     // 2: center={this.csuOvalGeographicCoordinates()} zoom={10}
     return (
-      <Map center={this.getCurrentCoordinates()} zoom={10}
+      <Map center={this.csuOvalGeographicCoordinates()} zoom={10}
            style={{height: 500, maxwidth: 700}}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
-        <Marker position={this.getCurrentCoordinates()}
-                icon={this.markerIcon()}>
-          <Popup className="font-weight-extrabold">{this.currentLocationPopup()}</Popup>
-        </Marker>
+        <Geolocation/>
       </Map>
     )
   }
@@ -81,38 +79,6 @@ export default class Home extends Component {
 
   getCurrentCoordinates() {
     return L.latLng(this.state.latitude, this.state.longitude);
-  }
-
-  currentLocationPopup() {
-      return this.state.location;
-  }
-
-  renderGeolocation() {
-    var options = {
-      enableHighAccuracy: true,
-      maximumAge: 0
-    };
-      try {
-        navigator.geolocation.getCurrentPosition(this.success, this.error, options);
-      }
-      catch (e) {
-
-      }
-
-  }
-
-  success(pos) {
-    var crd = pos.coords;
-
-    this.setState({latitude: crd.latitude, longitude: crd.longitude, location: 'your location'});
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-  }
-
-  error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
   }
 
   renderIntro() {
