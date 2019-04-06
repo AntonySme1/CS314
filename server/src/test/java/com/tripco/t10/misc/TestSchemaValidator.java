cab3229d74cf7a18b46d6bbe268f80e3eb8658f9
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-04-06 09:13:16 -0600

    added a test for valid api/config response, updated config schema to allow filters

--- server/src/test/java/com/tripco/t10/misc/TestSchemaValidator.java
+++ server/src/test/java/com/tripco/t10/misc/TestSchemaValidator.java
@@ -3,10 +3,10 @@ package com.tripco.t10.misc;
 import org.json.JSONObject;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class TestSchemaValidator {
+
     @Test
     public void configBadResponseSchemaTest() {
         SchemaValidator sv = new SchemaValidator();
@@ -14,4 +14,23 @@ public class TestSchemaValidator {
         boolean isValid = sv.performValidation(badConfig, "/TIPConfigSchema.json");
         assertFalse(isValid);
     }
+
+    @Test
+    public void configGoodResponseSchemaTest() {
+        String configResponse = "{" +
+                "\"requestType\"        : \"config\"," +
+                "\"requestVersion\"     : 4," +
+                "\"serverName\"         : \"t## name\"," +
+                "\"placeAttributes\"    : [\"name\", \"latitude\", \"longitude\", \"id\", \"municipality\", \"region\", \"country\", \"continent\", \"altitude\"]," +
+                "\"optimizations\"      : [\"none\", \"short\", \"shorter\"]," +
+                "\"filters\"            : [{\"name\": \"type\"," +
+                                            "\"values\": [\"airport\",\"heliport\",\"balloonport\",\"closed\"]}" +
+                                         "]," +
+                "}";
+        SchemaValidator sv = new SchemaValidator();
+        JSONObject goodConfig = new JSONObject(configResponse);
+        boolean isValid = sv.performValidation(goodConfig, "/TIPConfigSchema.json");
+        assertTrue(isValid);
+    }
 }
+
