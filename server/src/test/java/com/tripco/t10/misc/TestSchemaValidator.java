6e29645047890b26b0b3a156f8d7b7ac9c037d51
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-04-06 10:33:32 -0600

    created a test for a good find request, updated TIPFindSchema to include the narrow element

--- server/src/test/java/com/tripco/t10/misc/TestSchemaValidator.java
+++ server/src/test/java/com/tripco/t10/misc/TestSchemaValidator.java
@@ -24,8 +24,8 @@ public class TestSchemaValidator {
                 "\"placeAttributes\"    : [\"name\", \"latitude\", \"longitude\", \"id\", \"municipality\", \"region\", \"country\", \"continent\", \"altitude\"]," +
                 "\"optimizations\"      : [\"none\", \"short\", \"shorter\"]," +
                 "\"filters\"            : [{\"name\": \"type\"," +
-                                            "\"values\": [\"airport\",\"heliport\",\"balloonport\",\"closed\"]}" +
-                                         "]," +
+                "\"values\": [\"airport\",\"heliport\",\"balloonport\",\"closed\"]}" +
+                "]," +
                 "}";
         SchemaValidator sv = new SchemaValidator();
         JSONObject goodConfigResponse = new JSONObject(configResponse);
@@ -41,7 +41,7 @@ public class TestSchemaValidator {
                 "\"origin\"         : {\"latitude\":  \"40.6\", \"longitude\": \"-105.1\", \"name\":\"Fort Collins, Colorado, USA\"}," +
                 "\"destination\"    : {\"latitude\": \"-33.9\", \"longitude\":  \"151.2\", \"name\":\"Sydney, New South Wales, Australia\"}," +
                 "\"earthRadius\"    : 3958.8," +
-        "}";
+                "}";
         SchemaValidator sv = new SchemaValidator();
         JSONObject goodDistanceRequest = new JSONObject(distanceRequest);
         boolean isValid = sv.performValidation(goodDistanceRequest, "/TIPDistanceSchema.json");
@@ -54,16 +54,29 @@ public class TestSchemaValidator {
                 "\"requestType\"    : \"itinerary\"," +
                 "\"requestVersion\" : 4," +
                 "\"options\"        : { \"title\":\"My Trip\"," +
-                                       "\"earthRadius\":\"3958.8\"," +
-                                       "\"optimization\":\"none\" }," +
+                "\"earthRadius\":\"3958.8\"," +
+                "\"optimization\":\"none\" }," +
                 "\"places\"         : [{\"name\":\"Denver\",       \"latitude\": \"39.7\", \"longitude\": \"-105.0\"}," +
-                                      "{\"name\":\"Boulder\",      \"latitude\": \"40.0\", \"longitude\": \"-105.4\"}," +
-                                      "{\"name\":\"Fort Collins\", \"latitude\": \"40.6\", \"longitude\": \"-105.1\"}]" +
+                "{\"name\":\"Boulder\",      \"latitude\": \"40.0\", \"longitude\": \"-105.4\"}," +
+                "{\"name\":\"Fort Collins\", \"latitude\": \"40.6\", \"longitude\": \"-105.1\"}]" +
                 "}";
         SchemaValidator sv = new SchemaValidator();
         JSONObject goodItineraryRequest = new JSONObject(itineraryRequest);
         boolean isValid = sv.performValidation(goodItineraryRequest, "/TIPItinerarySchema.json");
         assertTrue(isValid);
     }
-}
 
+    @Test
+    public void findGoodRequestSchemaTest() {
+        String findRequest = "{" +
+                "\"requestType\"    :\"find\"," +
+                "\"requestVersion\" :4," +
+                "\"match\"          :\"fort collins\"," +
+                "\"narrow\"         :[]" +
+        "}";
+        SchemaValidator sv = new SchemaValidator();
+        JSONObject goodFindRequest = new JSONObject(findRequest);
+        boolean isValid = sv.performValidation(goodFindRequest, "/TIPFindSchema.json");
+        assertTrue(isValid);
+    }
+}
