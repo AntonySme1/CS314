a83a2e7798ce748b49713d2d3f2dc235256bbf44
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-04-06 11:17:32 -0600

    surround tests with try/catch to handle propagated exceptions

--- server/src/test/java/com/tripco/t10/misc/TestSchemaValidator.java
+++ server/src/test/java/com/tripco/t10/misc/TestSchemaValidator.java
@@ -9,74 +9,94 @@ public class TestSchemaValidator {
 
     @Test
     public void configBadResponseSchemaTest() {
-        SchemaValidator sv = new SchemaValidator();
-        JSONObject badConfigResponse = new JSONObject("{}");
-        boolean isValid = sv.performValidation(badConfigResponse, "/TIPConfigSchema.json");
-        assertFalse(isValid);
+        try {
+            SchemaValidator sv = new SchemaValidator();
+            JSONObject badConfigResponse = new JSONObject("{}");
+            boolean isValid = sv.performValidation(badConfigResponse, "/TIPConfigSchema.json");
+            assertFalse(isValid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void configGoodResponseSchemaTest() {
-        String configResponse = "{" +
-                "\"requestType\"        : \"config\"," +
-                "\"requestVersion\"     : 4," +
-                "\"serverName\"         : \"t## name\"," +
-                "\"placeAttributes\"    : [\"name\", \"latitude\", \"longitude\", \"id\", \"municipality\", \"region\", \"country\", \"continent\", \"altitude\"]," +
-                "\"optimizations\"      : [\"none\", \"short\", \"shorter\"]," +
-                "\"filters\"            : [{\"name\": \"type\"," +
-                "\"values\": [\"airport\",\"heliport\",\"balloonport\",\"closed\"]}" +
-                "]," +
-                "}";
-        SchemaValidator sv = new SchemaValidator();
-        JSONObject goodConfigResponse = new JSONObject(configResponse);
-        boolean isValid = sv.performValidation(goodConfigResponse, "/TIPConfigSchema.json");
-        assertTrue(isValid);
+        try {
+            String configResponse = "{" +
+                    "\"requestType\"        : \"config\"," +
+                    "\"requestVersion\"     : 4," +
+                    "\"serverName\"         : \"t## name\"," +
+                    "\"placeAttributes\"    : [\"name\", \"latitude\", \"longitude\", \"id\", \"municipality\", \"region\", \"country\", \"continent\", \"altitude\"]," +
+                    "\"optimizations\"      : [\"none\", \"short\", \"shorter\"]," +
+                    "\"filters\"            : [{\"name\": \"type\"," +
+                    "\"values\": [\"airport\",\"heliport\",\"balloonport\",\"closed\"]}" +
+                    "]," +
+                    "}";
+            SchemaValidator sv = new SchemaValidator();
+            JSONObject goodConfigResponse = new JSONObject(configResponse);
+            boolean isValid = sv.performValidation(goodConfigResponse, "/TIPConfigSchema.json");
+            assertTrue(isValid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void distanceGoodRequestSchemaTest() {
-        String distanceRequest = "{" +
-                "\"requestType\"    : \"distance\"," +
-                "\"requestVersion\" : 4," +
-                "\"origin\"         : {\"latitude\":  \"40.6\", \"longitude\": \"-105.1\", \"name\":\"Fort Collins, Colorado, USA\"}," +
-                "\"destination\"    : {\"latitude\": \"-33.9\", \"longitude\":  \"151.2\", \"name\":\"Sydney, New South Wales, Australia\"}," +
-                "\"earthRadius\"    : 3958.8," +
-                "}";
-        SchemaValidator sv = new SchemaValidator();
-        JSONObject goodDistanceRequest = new JSONObject(distanceRequest);
-        boolean isValid = sv.performValidation(goodDistanceRequest, "/TIPDistanceSchema.json");
-        assertTrue(isValid);
+        try {
+            String distanceRequest = "{" +
+                    "\"requestType\"    : \"distance\"," +
+                    "\"requestVersion\" : 4," +
+                    "\"origin\"         : {\"latitude\":  \"40.6\", \"longitude\": \"-105.1\", \"name\":\"Fort Collins, Colorado, USA\"}," +
+                    "\"destination\"    : {\"latitude\": \"-33.9\", \"longitude\":  \"151.2\", \"name\":\"Sydney, New South Wales, Australia\"}," +
+                    "\"earthRadius\"    : 3958.8," +
+                    "}";
+            SchemaValidator sv = new SchemaValidator();
+            JSONObject goodDistanceRequest = new JSONObject(distanceRequest);
+            boolean isValid = sv.performValidation(goodDistanceRequest, "/TIPDistanceSchema.json");
+            assertTrue(isValid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void itineraryGoodRequestSchemaTest() {
-        String itineraryRequest = "{" +
-                "\"requestType\"    : \"itinerary\"," +
-                "\"requestVersion\" : 4," +
-                "\"options\"        : { \"title\":\"My Trip\"," +
-                "\"earthRadius\":\"3958.8\"," +
-                "\"optimization\":\"none\" }," +
-                "\"places\"         : [{\"name\":\"Denver\",       \"latitude\": \"39.7\", \"longitude\": \"-105.0\"}," +
-                "{\"name\":\"Boulder\",      \"latitude\": \"40.0\", \"longitude\": \"-105.4\"}," +
-                "{\"name\":\"Fort Collins\", \"latitude\": \"40.6\", \"longitude\": \"-105.1\"}]" +
-                "}";
-        SchemaValidator sv = new SchemaValidator();
-        JSONObject goodItineraryRequest = new JSONObject(itineraryRequest);
-        boolean isValid = sv.performValidation(goodItineraryRequest, "/TIPItinerarySchema.json");
-        assertTrue(isValid);
+        try {
+            String itineraryRequest = "{" +
+                    "\"requestType\"    : \"itinerary\"," +
+                    "\"requestVersion\" : 4," +
+                    "\"options\"        : { \"title\":\"My Trip\"," +
+                    "\"earthRadius\":\"3958.8\"," +
+                    "\"optimization\":\"none\" }," +
+                    "\"places\"         : [{\"name\":\"Denver\",       \"latitude\": \"39.7\", \"longitude\": \"-105.0\"}," +
+                    "{\"name\":\"Boulder\",      \"latitude\": \"40.0\", \"longitude\": \"-105.4\"}," +
+                    "{\"name\":\"Fort Collins\", \"latitude\": \"40.6\", \"longitude\": \"-105.1\"}]" +
+                    "}";
+            SchemaValidator sv = new SchemaValidator();
+            JSONObject goodItineraryRequest = new JSONObject(itineraryRequest);
+            boolean isValid = sv.performValidation(goodItineraryRequest, "/TIPItinerarySchema.json");
+            assertTrue(isValid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void findGoodRequestSchemaTest() {
-        String findRequest = "{" +
-                "\"requestType\"    :\"find\"," +
-                "\"requestVersion\" :4," +
-                "\"match\"          :\"fort collins\"," +
-                "\"narrow\"         :[]" +
-        "}";
-        SchemaValidator sv = new SchemaValidator();
-        JSONObject goodFindRequest = new JSONObject(findRequest);
-        boolean isValid = sv.performValidation(goodFindRequest, "/TIPFindSchema.json");
-        assertTrue(isValid);
+        try {
+            String findRequest = "{" +
+                    "\"requestType\"    :\"find\"," +
+                    "\"requestVersion\" :4," +
+                    "\"match\"          :\"fort collins\"," +
+                    "\"narrow\"         :[]" +
+            "}";
+            SchemaValidator sv = new SchemaValidator();
+            JSONObject goodFindRequest = new JSONObject(findRequest);
+            boolean isValid = sv.performValidation(goodFindRequest, "/TIPFindSchema.json");
+            assertTrue(isValid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
