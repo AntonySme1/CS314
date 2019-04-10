7f7c58951d3fb6f45f9c14d589b05e1a0cc3f580
patrickkeleher <keleherpatrick1@gmail.com>
2019-04-09 21:19:13 -0600

    nearest neighbor algorithm

--- server/src/main/java/com/tripco/t10/TIP/TIPConfig.java
+++ server/src/main/java/com/tripco/t10/TIP/TIPConfig.java
@@ -66,45 +66,4 @@ public class TIPConfig extends TIPHeader {
 
 }
 
- /*public static Integer[] nearestNeighbor(Object[] inputPlaces)
-        {
-        double earthRadius = Double.parseDouble((String) this.options.get("earthRadius"));
-        long shortestTourLength = Integer.MAX_VALUE;
-        Integer[] shortestTourIndexes = new Integer[inputPlaces.length];
-        Integer[] currentTourIndex = new Integer[inputPlaces.length];
-        long[][] distances = GreatCircleDistance.getDistance(inputPlaces, shortestTourIndexes, earthRadius);
-        for(int startingCity = 0; startingCity < inputPlaces.length; ++startingCity)
-        {
-        int currentCityIndex = 0;
-        long currentTourLength = 0;
-        boolean[] visitedCities = new boolean[inputPlaces.length];
-        visitedCities[startingCity] = true;
-        currentTourIndex[currentCityIndex] = startingCity;
-        while(currentCityIndex < currentTourIndex.length-1)
-        {
-        long closestNeighborDistance = Integer.MAX_VALUE;
-        int closestNeighborIndex = -1;
-        for(int i = 0; i < inputPlaces.length; ++i)
-        {
-        long distanceToNeighbor = distances[currentTourIndex[currentCityIndex]][i];
-        if(!visitedCities[i] && distanceToNeighbor < closestNeighborDistance)
-        {
-        closestNeighborDistance = distanceToNeighbor;
-        closestNeighborIndex = i;
-        }
-        }
-        currentCityIndex=++currentCityIndex;
-        currentTourIndex[currentCityIndex] = closestNeighborIndex;
-        currentTourLength+=closestNeighborDistance;
-        visitedCities[closestNeighborIndex] = true;
-        }
-        currentTourLength+=distances[currentTourIndex[0]][currentTourIndex[currentTourIndex.length-1]];
-        log.info("Tour Length: " + currentTourLength);
-        if(currentTourLength < shortestTourLength){
-        shortestTourIndexes = currentTourIndex;
-        }
-        }
-        return shortestTourIndexes;
-        }
-        */
 
