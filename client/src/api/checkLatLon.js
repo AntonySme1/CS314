400baa9d6b447ce9076f27903f3b01adf46416d6
Saurav Bhuju <sbhuju@rams.colostate.edu>
2019-04-15 13:26:48 -0600

    added LatLon parsing in seperate file

---
+++ client/src/api/checkLatLon.js
@@ -0,0 +1,37 @@
+import coordParser from 'coord-parser';
+
+
+export const isValidLatLon = (lat,lon)=>{
+  const parsedLatLon = parseLatLon(lat,lon);
+  if (isParseValid(parsedLatLon)){
+    if (checkValidLatLonRange(parsedLatLon.lat,parsedLatLon.lon)){}
+    return true
+  }
+
+  return false;
+};
+
+
+
+export const parseLatLon = (lat,lon) => {
+  const latLon = `${lat}, ${lon}`;
+
+  let parsedLatLon = {};
+
+  try {
+    parsedLatLon = coordParser(latLon);
+  }
+  catch (e) {}
+  return parsedLatLon;
+
+};
+
+const isParseValid = (parsedLatLon) => {
+  return Object.keys(parsedLatLon).length === 2
+};
+
+export const checkValidLatLonRange = (lat,lon) =>{
+  const checkLatRange = parseFloat(lat) >= -90 && parseFloat(lat) <= 90;
+  const checkLonRange = parseFloat(lon) >= -180 && parseFloat(lon) <= 180;
+  return checkLatRange && checkLonRange;
+};
